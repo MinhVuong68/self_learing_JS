4c7abd77e473014f8706fd4184e29d1512fd70a7
nguyenminhvuong <minhvuong06082001@gmail.com>
2022-05-06 00:14:09 +0700

    lan2

--- Demo_ClassList/app.js
+++ Demo_ClassList/app.js
@@ -1,7 +1,10 @@
 var boxElement = document.querySelector('.box');
 
 // add
-
 setTimeout(()=>{
     boxElement.classList.add('red');
 },3000)
+
+setInterval(()=>{
+    boxElement.classList.toggle('red')
+},1000)
