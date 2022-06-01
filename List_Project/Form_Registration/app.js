aa21cc25dce5f10c4a7557edc6f1373e9aef4d06
nguyenminhvuong <minhvuong06082001@gmail.com>
2022-06-01 23:00:04 +0700

    finish event confirm password and save rules

--- List_Project/Form_Registration/app.js
+++ List_Project/Form_Registration/app.js
@@ -1,7 +1,16 @@
 function Validator(options) {
+    var selectorRules = {}
     function Validate(inputElement,rule){
         var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
-        var errorMessage = rule.test(inputElement.value)
+        var errorMessage
+
+        // Lay ra cac rule cua selector
+        var rules = selectorRules[rule.selector]
+
+        for(var i=0;i<rules.length;i++) {
+            errorMessage = rules[i](inputElement.value)
+            if(errorMessage) break
+        }
         if(errorMessage) {
             errorElement.innerText = errorMessage
             inputElement.parentElement.classList.add('invalid')
@@ -11,8 +20,24 @@ function Validator(options) {
         }
     }
     var formElement = document.querySelector(options.form)
+    formElement.onsubmit = function(e) {
+        e.preventDefault()
+        options.rules.forEach((rule) => {
+            var inputElement = formElement.querySelector(rule.selector)
+            Validate(inputElement,rule)
+        })
+    }
     if(formElement) {
         options.rules.forEach((rule) => {
+
+            // Luu lai cac rule cho moi input
+
+            if(Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test)
+            } else {
+                selectorRules[rule.selector] = [rule.test]
+            }
+
             var inputElement = formElement.querySelector(rule.selector)
             if(inputElement) {
                 inputElement.onblur = function() {
@@ -30,30 +55,40 @@ function Validator(options) {
 }
 
 
-Validator.isFullName = function(selector) {
+Validator.isRequired = function(selector,message) {
     return {
         selector: selector,
         test: function(value){
-            return value.trim() ? undefined : 'Họ và tên không được để trống!'
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này'
         }
     }
 }
 
-Validator.isEmail = function(selector) {
+Validator.isEmail = function(selector,message) {
     return {
         selector: selector,
         test: function(value){
             var regex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-            return regex.test(value) ? undefined : 'Email không đúng định dạng'
+            return regex.test(value) ? undefined : message || 'Email không đúng định dạng'
         }
     }
 }
 
-Validator.isPassword = function(selector,minLength) {
+Validator.minLength = function(selector,minLength,message) {
     return {
         selector: selector,
         test: function(value){
-            return value.length >= minLength ? undefined : 'Mật khẩu phải nhiều hơn 7 kí tự'
+            return value.length >= minLength ? undefined : message || `Vui lòng nhập từ ${minLength} kí tự trở lên`
+        }
+    }
+}
+
+
+Validator.isConfirmed = function(selector,getConfirmed,message) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value === getConfirmed() ? undefined : message || 'Giá trị nhập vào không chính xác!'
         }
     }
 }
